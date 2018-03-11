eee0f66fe0d76a077e7176dfe3468f738ef24dc5
Borgel <iborgel@gmail.com>
2018-03-11 12:47:24 -0700

    Add stubs for test mode files

---
+++ Firmware/Inc/test.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdbool.h>
+
+bool test_EnterTestMode(void);
+void test_DoTests(void);
+
---
+++ Firmware/Src/test.c
@@ -0,0 +1,14 @@
+#include "test.h"
+
+#include <stdbool.h>
+
+bool test_EnterTestMode(void) {
+   //TODO check if we should enter test mode
+
+   return false;
+}
+
+void test_DoTests(void) {
+   //TODO run tests
+}
+
