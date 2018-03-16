d01f15fee6bb98ee990bb81a5269ef26f6adad4b
Kerry Scharfglass <iborgel@gmail.com>
2018-03-15 22:21:18 -0700

    Leave IR TX configured as PP for now
    OD seems to maybe not work.

--- Firmware/Src/stm32f0xx_hal_msp.c
+++ Firmware/Src/stm32f0xx_hal_msp.c
@@ -187,7 +187,6 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
       GPIO_InitStruct.Pin = IR_TX_Pin;
       GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
       //GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
-      //GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
       GPIO_InitStruct.Pull = GPIO_NOPULL;
       GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
       GPIO_InitStruct.Alternate = GPIO_AF5_TIM17;
