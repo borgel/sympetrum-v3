77003f8638d1454c3435b42f3aa84e3533985d16
Kerry Scharfglass <iborgel@gmail.com>
2018-03-20 21:56:20 -0700

    Re-enable open drain mode for IR TX drive
    It works fine now that everything else is fixed.

--- Firmware/Src/stm32f0xx_hal_msp.c
+++ Firmware/Src/stm32f0xx_hal_msp.c
@@ -185,8 +185,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
 
       __HAL_RCC_GPIOA_CLK_ENABLE();
       GPIO_InitStruct.Pin = IR_TX_Pin;
-      GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-      //GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+      GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
       GPIO_InitStruct.Pull = GPIO_NOPULL;
       GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
       GPIO_InitStruct.Alternate = GPIO_AF5_TIM17;
