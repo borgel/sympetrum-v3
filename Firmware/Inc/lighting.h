18f02e3f07a5ff96aa611379258387cbcdb1b7d9
Kerry Scharfglass <iborgel@gmail.com>
2018-05-16 21:55:36 -0700

    Add stubs for lighting control

---
+++ Firmware/Inc/lighting.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <stdint.h>
+
+void lighting_Init(void);
+void lighting_Timeslice(uint32_t const timeMS);
+
+
---
+++ Firmware/Src/lighting.c
@@ -0,0 +1,9 @@
+#include "lighting.h"
+
+#include <stdint.h>
+
+void lighting_Init(void) {
+}
+
+void lighting_Timeslice(uint32_t const timeMS) {
+}
