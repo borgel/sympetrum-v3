da09a2fb74e6f7054cf410c60bcf61867677f022
Kerry Scharfglass <iborgel@gmail.com>
2018-06-02 10:13:52 -0700

    Add beacon module to manage sending and receiving IR beacons

---
+++ Firmware/Src/beacon.c
@@ -0,0 +1,65 @@
+#include "beacon.h"
+
+#include "ir.h"
+#include "board_id.h"
+#include "iprintf.h"
+
+#include <stdint.h>
+#include <stdio.h>
+
+#define BEACON_STR_LEN     (12)    //<<########>>
+
+static char const * const SpecialBeaconIDs[] = {
+   "12345678",
+   "12345704",
+};
+
+void beacon_Init(void) {
+   IRInit();
+}
+
+void beacon_Send(void) {
+   char beacon[BEACON_STR_LEN + 1] = {0};
+   snprintf(beacon, sizeof(beacon), "<<%08lX>>", bid_GetID());
+   // don't send the trailing null
+   IRTxBuff((uint8_t*)beacon, sizeof(beacon) - 1);
+
+   //FIXME rm
+   iprintf("%s", beacon);
+}
+
+enum BeaconStatus beacon_HaveReceived(void) {
+   if(IRDataReady()) {
+      //FIXME rm?
+      iprintf("Got Incoming IR Message ");
+
+      uint32_t bytes = 0;
+      uint8_t* buf = IRGetBuff(&bytes);
+
+      iprintf("%d bytes [%12s]\n", bytes, buf);
+
+      // check if it's valid at all beacon
+      if(bytes == 0 || buf == NULL) {
+         return BS_None;
+      }
+
+      // TODO switch on first char to see what it is (draw string, etc)
+
+      // check if it's a valid beacon
+      if(bytes != BEACON_STR_LEN) {
+         iprintf("Got %d bytes, not %d\n", bytes, BEACON_STR_LEN);
+         return BS_None;
+      }
+      if(!(buf[0] == '<' && buf[1] == '<' && buf[10] == '>' && buf[11] == '>')) {
+         iprintf("Beacon string formatted incorrectly\n");
+         return BS_None;
+      }
+
+      //TODO check if it's a special ID
+
+      return BS_Received;
+   }
+
+   return BS_None;
+}
+
