4c4950a8f20689db94246e59f1319150e50e6c90
Kerry Scharfglass <iborgel@gmail.com>
2018-06-23 14:27:21 -0700

    Show firmware version on LEDs at boot

--- Firmware/Src/main.c
+++ Firmware/Src/main.c
@@ -14,10 +14,13 @@
 #include "version.h"
 #include "test_patterns.h"
 
-#include "ir.h"
+// pull in the interpolator just for showing version on boot
+#include "yabi/yabi.h"
 
 #include <string.h>
 #include <stdlib.h>
+
+#define VERSION_FADE_SPEED_MS 500
 
 #define BUTTON_DEBOUNCE_MS    150
 static uint32_t lastUserButton = 0;
@@ -25,6 +28,8 @@ static uint32_t lastUserButton = 0;
 // indicate if we are in test mode
 static bool TestMode = false;
 static bool TestModeLED = false;
+
+static void VersionToLEDs(void);
 
 int main(void)
 {
@@ -47,6 +52,10 @@ int main(void)
    TestMode = false;
 
    lighting_Init();
+
+   // show version on LEDs before real animations take over
+   VersionToLEDs();
+
    pattern_Init();
 
    //TODO enter on boot if button held down
@@ -68,6 +77,41 @@ int main(void)
    }
 
    return 0;
+}
+/*
+ * Write this unit's SW version to the LEDs once.
+ */
+static void VersionToLEDs(void) {
+   struct color_ColorHSV c;
+
+   uint8_t const colorSet = rand();
+   uint8_t const colorCleared = rand();
+
+   iprintf("Displaying version on LEDs...");
+
+   //unpack each bit, and set the Blue LED channel to it
+   uint32_t mask = 0x01;
+   for(int i = 0; i < TOTAL_LOGICAL_LEDS; i++) {
+      // set the channel to 100 counts if the bit is set, 0 otherwise
+      c.h = (mask & FW_VERSION) ? colorSet: colorCleared;
+      c.s = 100;
+      c.v = 100;
+
+      lighting_DrawPixelLinear(i, &c, 0, VERSION_FADE_SPEED_MS);
+
+      // only move the mask if we are within the size of the version var
+      if(i < 8 * sizeof(FW_VERSION)) {
+         mask <<= 1;
+      }
+
+   }
+
+   uint32_t startTime = HAL_GetTick();
+   while(HAL_GetTick() - startTime < VERSION_FADE_SPEED_MS) {
+      yabi_giveTime(HAL_GetTick());
+   }
+
+   iprintf("Done\n");
 }
 
 void main_DoButton(bool const buttonPressed) {
